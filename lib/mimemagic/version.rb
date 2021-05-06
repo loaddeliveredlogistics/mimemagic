8395810d867a511540314458f973fb659cc7b58a
ajs5710 <asnider@loaddelivered.com>
2021-05-06 17:10:48 -0500

    Update version.rb

--- lib/mimemagic/version.rb
+++ lib/mimemagic/version.rb
@@ -1,5 +1,5 @@
 class MimeMagic
   # MimeMagic version string
   # @api public
-  VERSION = '0.3.0' # probably would be more appropriate to change this in a paperclip fork but this gem is smaller
+  VERSION = '0.3.0.bs' # probably would be more appropriate to change this in a paperclip fork but this gem is smaller
 end
